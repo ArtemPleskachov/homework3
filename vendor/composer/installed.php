a8812ec1e92cd6c85b29adf4840cb88ee4a63484
artempleskachov <pleskachov@icloud.com>
2022-10-23 15:51:37 +0300

    Add Mailcheker lib, and try to use it

---
+++ vendor/composer/installed.php
@@ -0,0 +1,62 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pleskahcov/php-pro',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f4ec9a5bf2829e840e1d5efc93d26d75a3cb0c37',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fgribreau/mailchecker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'aab498aeeb1fb9521f78b3cd99803aec667bd6f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fgribreau/mailchecker',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e68c00670e310e276da9624d75965f5d2f28aa80',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(
+                0 => '3.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'pleskahcov/php-pro' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f4ec9a5bf2829e840e1d5efc93d26d75a3cb0c37',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'fe5ea303b0887d5caefd3d431c3e61ad47037001',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(
+                0 => '3.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '3.0.0',
+            ),
+        ),
+    ),
+);
---
+++ vendor/fgribreau/mailchecker/go.mod
@@ -0,0 +1,3 @@
+module github.com/FGRibreau/mailchecker/v5
+
+go 1.13
