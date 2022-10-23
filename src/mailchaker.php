226117b4bb9899d15d6bc2319c3b5d9a5f341cc7
artempleskachov <pleskachov@icloud.com>
2022-10-23 15:29:48 +0300

    Add Mailcheker lib, and try to use it

---
+++ src/mailchaker.php
@@ -0,0 +1,28 @@
+<?php
+namespace Pleskachov\PhpPro;
+
+
+
+use MailCheker\MailChecker;
+
+require_once 'vendor/autoload.php';
+
+//include __DIR__."/MailChecker/platform/php/MailChecker.php";
+//
+//include __DIR__ . "/../MailChecker/platform/php/MailChecker.php";
+
+//if(!MailChecker::isValid('myemail@yopmail.com')){
+//    die('O RLY !');
+//}
+//
+//if(!MailChecker::isValid('myemail.com')){
+//    die('O RLY !');
+//}
+
+$email = 'pleskachov@icloud.com';
+
+if(!MailChecker::isValid($email)) {
+    echo 'Is email not valid';
+} else {
+    echo 'Is email valid';
+}
