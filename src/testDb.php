bad284be6bf24c373429f341092c32037f766646
artempleskachov <pleskachov@icloud.com>
2022-11-19 10:34:31 +0200

    Home work #8 add blocks try catch

--- src/testDb.php
+++ src/testDb.php
@@ -1,14 +1,23 @@
 <?php
 
-
+try {
 $dbh = new PDO("mysql:host=db_mysql;dbname=firstdb", "artist", "qwerty");
+}
+catch (PDOException $e) {
+    echo 'Підключення не вдалося';
+}
 
-$dbh->prepare("insert into `table`(status, name, password) values (?, ?, ?)")
-    ->execute([
-        '2',
-        'стілець', //не зрозумів чому не бажає зберігати так, як вказую
-        '341'
-    ]);
+try {
+    $dbh->prepare("insert into `table`(status, name, password) values (?, ?, ?)")
+        ->execute([
+            '2',
+            'стілець', //не зрозумів чому не бажає зберігати так, як вказую
+            '341'
+        ]);
+}
+catch (PDOException $e) {
+    echo 'Збереження не вдалося' . PHP_EOL;
+}
 
 
 $allNames = $dbh->query("select * from `table`")->fetchAll(PDO::FETCH_ASSOC);
